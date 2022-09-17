925ab74c86149ab38e1487b4aa34be84de78255a
“[Peter <pwdrysdale@gmail.com>
2022-09-16 23:59:39 -0400

    external requests and json

--- src/main.go
+++ src/main.go
@@ -15,6 +15,8 @@ func main() {
 
 	http.HandleFunc("/hello", helloHandler)
 
+	http.HandleFunc("/todos", handleTodos)
+
 	// post request
 	http.HandleFunc("/test", test)
 
@@ -27,6 +29,63 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
+}
+
+func handleTodos(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getTodos(w, r)
+	//case "POST":
+	//	addTodo(w, r)
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+	}
+}
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	// get todos from json placeholder
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+		return
+	}
+
+	//send the todos back to the client
+	type Todo struct {
+		UserId int    `json:"userId"`
+		Id     int    `json:"id"`
+		Title  string `json:"title"`
+		Done   bool   `json:"completed"`
+	}
+
+	// get body in desired format for use in go
+	todos := make([]Todo, 0, 10)
+	err = json.NewDecoder(resp.Body).Decode(&todos)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// get ready to send as json and send it
+	marshalled, ok := json.Marshal(todos)
+	if ok != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(ok.Error()))
+		return
+	} else {
+		fmt.Fprintf(w, "%v", string(marshalled))
+	}
+
 }
 
 type test_struct struct {
